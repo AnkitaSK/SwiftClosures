40c96c423a632c81ed269d67c06a0f62996363f0
AnkitaSK <ankitakalangutkar90@gmail.com>
2021-04-09 21:12:19 +0200

    SwiftHigherOrderFunctionPractice

---
+++ SwiftHigherOrderFunctionPractice/SwiftHigherOrderFunctionPractice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftHigherOrderFunctionPractice
+//
+//  Created by Ankita Kalangutkar on 09/04/21.
+//  Copyright © 2021 Ankita Kalangutkar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
