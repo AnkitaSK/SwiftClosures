de931cd55b66e06e1dfdb7ddd24fb75128bdf883
AnkitaSK <ankitakalangutkar90@gmail.com>
2021-04-28 10:36:19 +0200

    Initial commit

---
+++ SwiftHigherOrderFunctionPractice/SwiftHigherOrderFunctionPractice/DemoOnClosures.swift
@@ -0,0 +1,9 @@
+//
+//  DemoOnClosures.swift
+//  SwiftHigherOrderFunctionPractice
+//
+//  Created by Ankita Kalangutkar on 28/04/21.
+//  Copyright © 2021 Ankita Kalangutkar. All rights reserved.
+//
+
+import Foundation
